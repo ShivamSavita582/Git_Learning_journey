13f244bc087e80421bcf34469782e7fbc4c4da64
Shivam <shivamsavitamahewa7068@gmail.com>
2025-12-24 11:45:00 +0530

    Index ko add kiya ha

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log('this is console window');
+
+for(let i= 0; i <=5; i ++){
+    console.log('my loop is running')
+}
+
+function hello(){
+    console.log('my function is working')
+}
+hello()
+
+let sayName = (fName ='uday',lName='singh')=>{
+    console.log('hello mitro',fName,lName)
+}
+sayName('uday','singh')
+sayName('anjali','dabi')
+sayName('anjali',)
+
+let student =['ram','shyam','anuj','priya'];
+console.log(student[2])
+console.log(student)
+
+let user = {
+    name:'peter',
+    age:24,
+    city:'pune'
+
+}
+console.log(user)
