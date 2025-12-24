e84a901fd7b1b2457d9bbd2c842627c81f26aa5c
Shivam <shivamsavitamahewa7068@gmail.com>
2025-12-24 12:11:05 +0530

    create add new file

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("welcome to web dev mastery course")
+
+let user = ['adam','sam','peter','radha','laxman']
+console.log(user)
+console.log(user.length)
+
+let profile ={
+    name :'Arjun',
+    age:25,
+    followers:'50K',
+    post :100,
+    profession :'Blogger,Youtuber'
+}
+console.log(profile)
+console.log(profile.post)
